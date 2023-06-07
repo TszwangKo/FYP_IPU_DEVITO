9f5c7f846e5ad556b0759b20f1abe8b1992a2484
TszWangKo <kotszwang@gmail.com>
2023-06-07 12:01:56 +0000

    :white_check_mark: Benchmark added for HeatEquation3D

--- IPU/HeatEquation3D/tests_3D.sh
+++ IPU/HeatEquation3D/tests_3D.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-rm -f 3D_test.log
 
-for ipu_no in 1
+side(){
+    if [ $1 == 1 ]; then
+        SIDE=360
+    elif [ $1 == 2 ]; then
+        SIDE=403
+    elif [ $1 == 4 ]; then
+        SIDE=508
+    elif [ $1 == 8 ]; then
+        SIDE=640
+    elif [ $1 == 16 ]; then
+        SIDE=806
+    else
+        SIDE=0
+        echo -ne "$SIDE not supported\n"
+    fi
+}
+
+for ipu_no in 2 4
 do
+    side $ipu_no
     printf "No of IPU: ${ipu_no}\n ================================================== \n" >> 3D_test.log
-    ./main --num-ipus ${ipu_no} >> 3D_test.log
+    ./main --num-ipus ${ipu_no} --depth $SIDE --height $SIDE --width $SIDE>> 3D_test.log
 done
